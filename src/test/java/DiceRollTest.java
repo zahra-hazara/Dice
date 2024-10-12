8d1d55e74e36d620351fafdf4bb0b6b71308f29c
zahra <zahra.hazara@metropolia.fi>
2024-10-12 22:15:50 +0300

    update

--- src/test/java/DiceRollTest.java
+++ src/test/java/DiceRollTest.java
@@ -1,5 +1,6 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DiceRollTest {
 
